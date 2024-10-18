330b225805b428cc9cfd716efc2d290af52a2ed8
KajetanPoliak <kpoliak@deloittece.com>
2024-10-18 21:37:16 +0200

    save keyword index results

--- source/pipeline.py
+++ source/pipeline.py
@@ -14,13 +14,12 @@ class Pipeline:
     def run(self) -> None:
         # Vectorize event data
         title_body_list = self.dp.get_email_data()
-        embeddings = apply_vectorization(title_body_list=title_body_list)
+        # embeddings = apply_vectorization(title_body_list=title_body_list)
 
         ####### KEYWORD SEARCH INDEX #######
         # Load the search index
         index = KeywordSearchIndex.from_file("keyword_search_index.json")
         print("Search index loaded from file:", index.index)
-
         # Define a query document
         query = {
             "title": title_body_list[0],
@@ -28,20 +27,18 @@ class Pipeline:
         }
         # Process a query document
         query_processed = index.process_query_document(**query)
-
         # Perform search
         search_results = index.search(query_processed, title=query["title"])
-
         # Display search results
         print(f"Query: {query!r}")
         index.display_search_results(search_results)
-
-        ##### FAISS INDEX #####
-        # Load the index
-        index = FaissIndex.from_file("faiss_ip.index")
-        dist, idx = index.search(embeddings, k=5)
-        print("Search after the load:", dist, idx)
-        assert idx[0][0] == 0 and np.isclose(dist[0][0], 1.0, atol=1e-3)
+        index.save_results(search_results, "keyword_search_results.csv")
+        # ##### FAISS INDEX #####
+        # # Load the index
+        # index = FaissIndex.from_file("faiss_ip.index")
+        # dist, idx = index.search(embeddings, k=5)
+        # print("Search after the load:", dist, idx)
+        # assert idx[0][0] == 0 and np.isclose(dist[0][0], 1.0, atol=1e-3)
 
 
 if __name__ == "__main__":
