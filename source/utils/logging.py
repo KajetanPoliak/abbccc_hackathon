753b09c47313ee392d5411858025bd49ede6f213
Josef Matějka <posledniamir@tuta.io>
2024-10-18 13:06:33 +0200

    fixing back

--- source/utils/logging.py
+++ source/utils/logging.py
@@ -12,7 +12,7 @@ T = TypeVar("T")
 
 @dataclass(init=False, frozen=True)
 class LoggingConfig:
-    verbosity_mapping = {0: ERROR, 1: WARNING, 2: INFO, 3: DEBUG  }
+    verbosity_mapping = {0: ERROR, 1: WARNING, 2: INFO, 3: DEBUG}
     verbosity: int = 3
     stream_format: str = "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
     datetime_format: str = "%Y-%m-%dT%H:%M:%S%z"
