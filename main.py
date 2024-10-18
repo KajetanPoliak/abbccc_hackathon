f3dabd370df608b3753cdece4efa10bc61c9eeb5
KajetanPoliak <kpoliak@deloittece.com>
2024-10-19 00:47:09 +0200

    add enrich to pipeline

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 from pathlib import Path
 from typing import Any, Dict, List
 
+from source.data_processing.data_processing import DataProcessor
 from source.pipeline import Pipeline
 
 __here__ = Path(__file__).resolve().parent
@@ -17,7 +18,10 @@ def run() -> List[Dict[str, Any]]:
 if __name__ == "__main__":
     res = run()
 
+    dp = DataProcessor()
+    full_result = dp.enrich_result(data=res)
+
     # Save the results to a file
     with open(__data_dir__ / "data_results.json", "w") as file:
-        res_json = json.dumps(res, indent=4)
+        res_json = json.dumps(full_result, indent=4)
         file.write(res_json)
