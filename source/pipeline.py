8cb09e1bda5f149a24f45d35f780d19f6393aa4f
KajetanPoliak <kpoliak@deloittece.com>
2024-10-18 21:53:35 +0200

    Add context search to pipeline

--- source/pipeline.py
+++ source/pipeline.py
@@ -3,7 +3,7 @@ import numpy as np
 from source.data_processing.data_processing import DataProcessor
 from source.index.keyword_index import KeywordSearchIndex
 from source.index.vector_index import FaissIndex
-from source.vectorization.deep import apply_vectorization
+from source.vectorization.deep import get_tokenizer_and_model
 
 
 class Pipeline:
@@ -14,31 +14,42 @@ class Pipeline:
     def run(self) -> None:
         # Vectorize event data
         title_body_list = self.dp.get_email_data()
-        # embeddings = apply_vectorization(title_body_list=title_body_list)
 
         ####### KEYWORD SEARCH INDEX #######
         # Load the search index
-        index = KeywordSearchIndex.from_file("keyword_search_index.json")
-        print("Search index loaded from file:", index.index)
+        keyword_index = KeywordSearchIndex.from_file(
+            "keyword_search_index.json"
+        )
+        print("Search index loaded from file:", keyword_index.index)
         # Define a query document
-        query = {
+        keyword_query = {
             "title": title_body_list[0],
             "body": title_body_list[1],
         }
         # Process a query document
-        query_processed = index.process_query_document(**query)
+        keyword_query_processed = keyword_index.process_query_document(
+            **keyword_query
+        )
         # Perform search
-        search_results = index.search(query_processed, title=query["title"])
+        search_results = keyword_index.search(
+            keyword_query_processed, title=keyword_query["title"]
+        )
         # Display search results
-        print(f"Query: {query!r}")
-        index.display_search_results(search_results)
-        index.save_results(search_results, "keyword_search_results.csv")
-        # ##### FAISS INDEX #####
-        # # Load the index
-        # index = FaissIndex.from_file("faiss_ip.index")
-        # dist, idx = index.search(embeddings, k=5)
-        # print("Search after the load:", dist, idx)
-        # assert idx[0][0] == 0 and np.isclose(dist[0][0], 1.0, atol=1e-3)
+        print(f"Query: {keyword_query!r}")
+        keyword_index.display_search_results(search_results)
+        keyword_index.save_results(search_results, "keyword_search_results.csv")
+
+        ##### FAISS INDEX #####
+        # Load the index
+        context_index = FaissIndex.from_file("faiss_ip.index")
+        context_query = context_index.process_query_document(
+            title=title_body_list[0], body=title_body_list[1]
+        )
+        tok, mod = get_tokenizer_and_model(device="mpu")
+        dist, idx, itms = context_index.search(
+            document=context_query, tokenizer=tok, model=mod, k=5
+        )
+        print("Search after the load:", dist, idx, itms)
 
 
 if __name__ == "__main__":
