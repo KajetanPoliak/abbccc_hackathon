80415134b8aff3a24effdfb75066ab7c42c44c62
KajetanPoliak <kpoliak@deloittece.com>
2024-10-18 22:40:05 +0200

    Load event data

--- source/pipeline.py
+++ source/pipeline.py
@@ -12,44 +12,46 @@ class Pipeline:
         self.index = FaissIndex(dim=768)
 
     def run(self) -> None:
-        # Vectorize event data
-        title_body_list = self.dp.get_email_data()
-
-        ####### KEYWORD SEARCH INDEX #######
-        # Load the search index
+        ### Initialize the pipeline ###
+        # Load the keyword index
         keyword_index = KeywordSearchIndex.from_file(
             "keyword_search_index.json"
         )
-        print("Search index loaded from file:", keyword_index.index)
-        # Define a query document
-        keyword_query = {
-            "title": title_body_list[0],
-            "body": title_body_list[1],
-        }
-        # Process a query document
-        keyword_query_processed = keyword_index.process_query_document(
-            **keyword_query
-        )
-        # Perform search
-        search_results = keyword_index.search(
-            keyword_query_processed, title=keyword_query["title"]
-        )
-        # Display search results
-        print(f"Query: {keyword_query!r}")
-        keyword_index.display_search_results(search_results)
-        keyword_index.save_results(search_results, "keyword_search_results.csv")
-
-        ##### FAISS INDEX #####
-        # Load the index
+        # Load the context index
         context_index = FaissIndex.from_file("faiss_ip.index")
-        context_query = context_index.process_query_document(
-            title=title_body_list[0], body=title_body_list[1]
-        )
-        tok, mod = get_tokenizer_and_model(device="mpu")
-        dist, idx, itms = context_index.search(
-            document=context_query, tokenizer=tok, model=mod, k=5
-        )
-        print("Search after the load:", dist, idx, itms)
+
+        # Get the event data
+        title_body_list = self.dp.get_email_data()
+        for event in title_body_list:
+            event_id = event[0]
+            ####### KEYWORD SEARCH #######
+            # Define a query document
+            keyword_query = {
+                "title": event[1],
+                "body": event[2],
+            }
+            # Process a query document
+            keyword_query_processed = keyword_index.process_query_document(
+                **keyword_query
+            )
+            # Perform search
+            search_results = keyword_index.search(
+                keyword_query_processed, title=keyword_query["title"]
+            )
+            search_results_df = keyword_index.to_dataframe(search_results)
+            search_results_df["event_id"] = event_id
+
+            print(search_results_df)
+
+            ##### FAISS INDEX #####
+            context_query = context_index.process_query_document(
+                title=event[1], body=event[2]
+            )
+            tok, mod = get_tokenizer_and_model(device="cpu")
+            dist, idx, itms = context_index.search(
+                document=context_query, tokenizer=tok, model=mod, k=5
+            )
+            print("Search after the load:", dist, idx, itms)
 
 
 if __name__ == "__main__":
